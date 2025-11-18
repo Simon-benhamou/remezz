2e2a17d702e175f28faf6e54bfa4088f2a0a54b6
Simon-benhamou <simon.benhamou@icloud.com>
2025-11-18 14:30:35 +0200

    refactor: remove Cypress testing scripts from package.json

--- frontend/cypress.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'cypress';
-
-export default defineConfig({
-  video: false,
-  e2e: {
-    baseUrl: process.env.CYPRESS_BASE_URL || 'http://localhost:5173',
-    supportFile: 'cypress/support/e2e.ts',
-  },
-});
