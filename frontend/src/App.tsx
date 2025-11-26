6457034123708a047640d29b0f2bd6ee323352e1
Simon-benhamou <simon.benhamou@icloud.com>
2025-11-26 22:28:21 +0200

    feat: Remove portfolio route from AuthenticatedApp to streamline navigation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -275,7 +275,6 @@ function AuthenticatedApp() {
             <Route path='/agents' element={<SessionsPage />} />
             <Route path='/ledger' element={<ExecutionLedgerPageNew />} />
             <Route path='/reports' element={<ReportsPage />} />
-            <Route path='/portfolio' element={<SessionsPage />} />
             <Route path='*' element={<Navigate to='/operations' replace />} />
           </Routes>
         </Content>
